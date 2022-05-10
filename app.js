a89d24f65abaa7dfa26d5f67dd10adc03d75ffc1
messiJod <88767533+messiJod@users.noreply.github.com>
2022-05-10 13:29:43 +0530

    parameter name changed

--- app.js
+++ app.js
@@ -2,22 +2,23 @@
 
 let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function rotateArrayLeft(array, d, n) {
+function rotateArrayLeft(array, elements, n) {
   let temp = [];
-  for (let i = 0; i < d; i++) {
+  for (let i = 0; i < elements; i++) {
     temp.push(array[i]);
   }
 
   //   rest of array forward moved
-  for (let i = 0; i < n - d; i++) {
-    array[i] = array[d + i];
+  for (let i = 0; i < n - elements; i++) {
+    array[i] = array[elements + i];
   }
   let j = 0;
-  for (let i = n - d; i < n; i++) {
+  for (let i = n - elements; i < n; i++) {
     array[i] = temp[j];
     j++;
   }
 }
 
+console.log(`Array before Rotation ${myArray}`);
 rotateArrayLeft(myArray, 2, myArray.length);
-console.log(myArray);
+console.log(`Array after Rotation  ${myArray}`);

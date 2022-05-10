cf76d39bc0afba18a856f4ff5395e0e6a262d560
messiJod <88767533+messiJod@users.noreply.github.com>
2022-05-10 13:13:12 +0530

    array left rotation

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-let obj = {};
+// Array left Rotation
 
-obj.name = "Jod";
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log(obj.this);
+function rotateArrayLeft(array, d, n) {
+  let temp = [];
+  for (let i = 0; i < d; i++) {
+    temp.push(array[i]);
+  }
+
+  //   rest of array forward moved
+  for (let i = 0; i < n - d; i++) {
+    array[i] = array[d + i];
+  }
+  let j = 0;
+  for (let i = n - d; i < n; i++) {
+    array[i] = temp[j];
+    j++;
+  }
+}
+
+rotateArrayLeft(myArray, 2, myArray.length);
+console.log(myArray);
